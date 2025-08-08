69a001bcddaa0ed93578f8afcc8b98ec5d490c76
muhammedarshadtp <arshadtp89@gmail.com>
2025-08-08 15:30:01 +0530

    Add linear search implementation using for and while loops
    - Implemented linear search with a for loop to iterate through the list
    - Reimplemented the same logic using a while loop
    - Checked for presence of target value and returned its index
    - Added print statements to show whether the element was found or not

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,1 +1,46 @@
 
+# ----------     using for loop. --------
+
+# def Linear_Search(arr,target):
+
+#     for i in range(len(arr)):
+#         if arr[i] == target:
+#             return i
+#     return -1
+
+
+# list=[1,2,3,4,5,6,7,8]
+
+# target=5
+
+# sum=Linear_Search(list,target)
+
+# if sum != -1:
+#     print("fount at :",sum)
+# else:
+#     print("not found")
+
+
+# ---------   using whileLoop   -----
+
+
+def Linear_Search(arr,target):
+
+    i=0
+
+    while i< len(arr):
+        if arr[i] == target:
+            return i
+        i +=1
+    return -1
+
+list=[1,2,4,5,6,3,7,8]
+target=9
+
+sum= Linear_Search(list,target)
+
+if sum != -1:
+    print("found at:",sum)
+else:
+    print("not found")    
+
